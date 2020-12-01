4b1a09f2405049168d23371915dd5c57e954b02f
enfy <enfy1337@gmail.com>
2020-12-01 19:27:56 +0100

    day 1

---
+++ day1.rb
@@ -0,0 +1,24 @@
+input = File.open("day1_input.txt")
+    @nums = []
+    input.each_with_index do |num, i|
+        @nums[i] = num.chomp.to_i
+    end
+
+def part1()
+    @nums.permutation(2).to_a.each do |tuple|
+        if tuple[0] + tuple[1] == 2020
+            puts tuple[0] * tuple[1]
+        end
+    end
+end
+
+def part2()
+    @nums.permutation(3).to_a.each do |triple|
+        if triple[0] + triple[1] + triple[2] == 2020
+            puts triple[0] * triple[1] * triple[2]
+        end
+    end
+end
+
+#part1
+#part2
