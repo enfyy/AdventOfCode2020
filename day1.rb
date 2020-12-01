4966783432e98e07854ca61fde824306de89c30f
enfy <enfy1337@gmail.com>
2020-12-01 19:32:48 +0100

    indentation oopsie

--- day1.rb
+++ day1.rb
@@ -1,8 +1,8 @@
 input = File.open("day1_input.txt")
-    @nums = []
-    input.each_with_index do |num, i|
-        @nums[i] = num.chomp.to_i
-    end
+@nums = []
+input.each_with_index do |num, i|
+    @nums[i] = num.chomp.to_i
+end
 
 def part1()
     @nums.permutation(2).to_a.each do |tuple|
@@ -11,7 +11,6 @@ def part1()
         end
     end
 end
-
 def part2()
     @nums.permutation(3).to_a.each do |triple|
         if triple[0] + triple[1] + triple[2] == 2020
